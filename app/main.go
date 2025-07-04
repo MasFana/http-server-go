5e150a27df3a6ef9074f1aed73b37fcc61bf9d04
Phisict <adrianfathan69@gmail.com>
2025-07-05 01:29:43 +0700

    Simple test parse

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
@@ -29,8 +30,23 @@ func main() {
 
 		if err != nil {
 			fmt.Println("Failed to accept connection")
+			conn.Close()
+			continue
 		}
-
+		req := make([]byte, 1024)
+		_, err = conn.Read(req)
+		if err != nil {
+			fmt.Println("Failed to read request")
+			conn.Close()
+			continue
+		}
+		fmt.Println(strings.Split(string(req), "\n")[0][:6])
+		if strings.Split(string(req), "\n")[0][:6] != "GET / " {
+			fmt.Println("Invalid request")
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			conn.Close()
+			continue
+		}
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		conn.Close()
 	}

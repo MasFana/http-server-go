da8ad24d23d7a1f8fec140267252b63482ffb9f8
Phisict <adrianfathan69@gmail.com>
2025-07-05 02:06:13 +0700

    test

--- app/main.go
+++ app/main.go
@@ -12,43 +12,52 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	defer l.Close()
+	defer listener.Close()
 
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 
 		if err != nil {
 			fmt.Println("Failed to accept connection")
-			conn.Close()
-			continue
+			return
 		}
-		req := make([]byte, 1024)
-		_, err = conn.Read(req)
-		if err != nil {
-			fmt.Println("Failed to read request")
-			conn.Close()
-			continue
-		}
-		fmt.Println(strings.Split(string(req), "\n")[0][:6])
-		if strings.Split(string(req), "\n")[0][:6] != "GET / " {
-			fmt.Println("Invalid request")
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			conn.Close()
-			continue
-		}
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		conn.Close()
+		go func(c net.Conn) {
+			defer c.Close()
+
+			req := make([]byte, 1024)
+			_, err = c.Read(req)
+			if err != nil {
+				fmt.Println("Failed to read request")
+				c.Close()
+				return
+			}
+
+			uri := parseUri(req)
+			fmt.Println(uri)
+			switch uri {
+			case "/":
+			default:
+				fmt.Println("Invalid request")
+				c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				c.Close()
+				return
+			}
+			response := "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Welcome to the Go HTTP Server!</h1></body></html>"
+			c.Write([]byte(response))
+		}(conn)
 	}
 
+}
+
+func parseUri(req []byte) string {
+	uri := strings.Split(string(req), "\n")[0]
+	uri = uri[4 : len(uri)-10] // Get only path from the request header
+	return uri
 }
